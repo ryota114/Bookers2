726fc0ebc65cf7f901f88f3ab56eb5f0efdd38a0
ryota114 <namahann0978@gmail.com>
2021-03-27 13:18:57 +0000

    [Update]4-10名前ログインの適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # deviseを使用する際にURLとしてusersを含む
+  devise_for :users
+
+  root to: "homes#top"
 end
