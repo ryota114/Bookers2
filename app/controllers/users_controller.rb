673e5188d6e35d7b2ccd6d3fc87964b6bd58beb7
ryota114 <namahann0978@gmail.com>
2021-03-30 10:44:15 +0000

    [Update]4-10signup,loginページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,9 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def index
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def update
   end
 
   private
