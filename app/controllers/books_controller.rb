7c7a0bdc411eb0fbe39503a47bf6b377b4d01f5f
ryota114 <namahann0978@gmail.com>
2021-04-03 03:20:21 +0000

    [update]4-10 flashの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     # その情報をsave
     @book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id), notice: "You have created book successfully."
   end
 
   def index
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book)
+    redirect_to book_path(@book), notice: "You have updated book successfully."
   end
 
   def destroy
