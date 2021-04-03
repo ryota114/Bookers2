49eb6d8fe589e29df180cce4ee5fd6790bbf32d9
ryota114 <namahann0978@gmail.com>
2021-04-03 01:49:40 +0000

    [Update]4-10CRUD実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @users = User.all
+    @book = Book.new
 
   end
 
@@ -11,15 +14,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introfuction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b61f349a63c6307b7ea8b5e60f3294eff747cc1de99643d668821b0886a41c434d7231e6d027accd533f3390fc4deabc205eefacd3b662dd6bbff64e7b1a994a'
